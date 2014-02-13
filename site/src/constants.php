3dbde5c0c4712c3189aa6de02dd8751a40902684
Romain CHABERNAUD <chabernaud.romain@gmail.com>
2014-02-13 19:38:50 +0100

    Add test package

--- site/src/constants.php
+++ site/src/constants.php
@@ -7,8 +7,8 @@ const DATE_PATTERN = 'd/m/Y H:i';
 
 //const ROOT_SERVER_DIRECTORY = '/dev/c/c'; // Used to know the free available space
 const ROOT_SERVER_DIRECTORY = '/'; // Used to know the free available space
-const FILES_SERVER_DIRECTORY = '../src/files-server/'; // Directory used to store empty files corresponding to files store on your seedbox
-const FILES_TO_DOWNLOAD_SERVER_DIRECTORY = '../src/files-to-download/'; // Directory used to store files we want to download on your seedbox
-const LOGS_DIRECTORY = '../src/logs/';
-const DOWNLOAD_DIRECTORY = '../src/download/';
+const FILES_SERVER_DIRECTORY = '../test/files-server/'; // Directory used to store empty files corresponding to files store on your seedbox
+const FILES_TO_DOWNLOAD_SERVER_DIRECTORY = '../test/files-to-download/'; // Directory used to store files we want to download on your seedbox
+const LOGS_DIRECTORY = '../test/logs/';
+const DOWNLOAD_DIRECTORY = '../test/download/';
 const SEEDBOX_NAME = 'XXX.seedbox.fr';
