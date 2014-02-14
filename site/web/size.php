b146e55cc5579d11c7a0ebfdc6a10c9e623c2cb8
Romain CHABERNAUD <chabernaud.romain@gmail.com>
2014-02-14 21:47:19 +0100

    Fix unused constant variable and remove test package

--- site/web/size.php
+++ site/web/size.php
@@ -5,11 +5,9 @@ include('../src/constants.php');
 include('../src/utils.php');
 if (empty($file)) {
     http_response_code(400);
-} else if (!file_exists(FILES_TO_DOWNLOAD_SERVER_DIRECTORY . SEEDBOX_NAME . '/' . $file)) {
-    // Do nothing
 } else {
     $decodedFile = urldecode($file);
-    $size = shell_exec('du -sk ' . FILES_TO_DOWNLOAD_SERVER_DIRECTORY . SEEDBOX_NAME . '/' . $decodedFile . ' | awk \'{print$1}\'') * 1024;
+    $size = shell_exec('du -sk ' . DOWNLOAD_DIRECTORY . SEEDBOX_NAME . '/' . $decodedFile . ' | awk \'{print$1}\'') * 1024;
     $filesDetails = json_decode(file_get_contents(FILES_DETAILS_MIRROR_SEEDBOX), true);
     foreach($filesDetails as $fileDetail) {
         if($fileDetail['file'] == $decodedFile) {
